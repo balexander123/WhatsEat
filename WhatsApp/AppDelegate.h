71d839d9b40e25ba831dc3fd25ba8d5a1a4ad531
Barry Alexander <barry_alexander@gap.com>
2015-05-24 22:33:23 -0700

    Initial Commit

---
+++ WhatsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhatsApp
+//
+//  Created by Barry Alexander on 5/24/15.
+//  Copyright (c) 2015 Barry Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhatsApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhatsApp
+//
+//  Created by Barry Alexander on 5/24/15.
+//  Copyright (c) 2015 Barry Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
