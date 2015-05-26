3966b307dc757122a55895809c596ab4f5d216be
Barry Alexander <barry.alexander@gmail.com>
2015-05-25 22:09:46 -0700

    [BA] add facebook login

--- WhatsApp/ViewController.m
+++ WhatsApp/ViewController.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 Barry Alexander. All rights reserved.
 //
 
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -17,6 +19,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+    loginButton.center = self.view.center;
+    [self.view addSubview:loginButton];
 }
 
 - (void)didReceiveMemoryWarning {
